29888f0e0ec1e7156bf604d24709475f1c844064
Shruti Morbale <shrutimorbale@gmail.com>
2026-03-17 14:09:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// function deleteThePost() {
+//     let isConfirmed = window.confirm("Are you sure you want to delete?");
+//    let element = document.getElementById("card"); 
+
+//     if (isConfirmed) {
+//         element.innerHTML = "Post deleted";
+//     } 
+// }
